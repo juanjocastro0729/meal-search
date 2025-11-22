eb385e01cf3b9799c5a60ee57c7a4ddbe1164e37
Juanjo Castro Rodriguez <juanjocastro0729@gmail.com>
2025-11-21 23:21:23 -0600

    Navigation is working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,8 +14,8 @@ onMounted(async () => {
 </script>
 
 <template>
- <div class="p-8 text-orange-500">
-    <h1 clas="text-4xl font-bold mb-4"> Random Meals
+ <div class="p-8 pb-0 text-orange-500">
+    <h1  class="text-4xl font-bold mb-4"> Random Meals
     </h1>
  </div>
  <Meals :meals="meals"></Meals>
